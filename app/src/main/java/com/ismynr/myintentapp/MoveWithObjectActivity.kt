ebf4723e79e1fbb06817e4fa52603d3288970d70
ismynr <gekkai7is.my@gmail.com>
2020-09-24 10:23:17 +0700

    implicit intent

--- app/src/main/java/com/ismynr/myintentapp/MoveWithObjectActivity.kt
+++ app/src/main/java/com/ismynr/myintentapp/MoveWithObjectActivity.kt
@@ -8,6 +8,7 @@ class MoveWithObjectActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_PERSON = "extra_person"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_object)
